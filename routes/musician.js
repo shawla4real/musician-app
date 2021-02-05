af1a1fc3fdc8702eb5fbe4d0d8089f1bcfdf4685
shawla4real <65089196+shawla4real@users.noreply.github.com>
2021-02-04 19:05:51 -0500

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -5,8 +5,8 @@ const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
 // healthcheck
-router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+router.get('/healthcheck', (req, res) => {
+  res.status('200').send("Status: okay!");
 });
 
 // retrieve all musicians from data store
